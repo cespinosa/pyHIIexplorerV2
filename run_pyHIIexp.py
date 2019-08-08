dcf45bcfd1601686cecb5119ffeef4449cf22924
cespinosa <cespinosa@astro.unam.mx>
2019-08-08 18:42:49 -0500

    Working script to run pyHIIexplorer

--- run_pyHIIexp.py
+++ run_pyHIIexp.py
@@ -15,5 +15,12 @@ ny = hdr['NAXIS2']
 XC, YC = get_center(obj_name, log_level=log_level)
 
 max_dist = 5.5
+frac_peak = 0.05
+F_max = 0.3
+dist = 0
+min_flux = 0.05
+output_path = '/home/espinosa/'
 
-a = pyHIIexplorer(Ha_map, nx, ny, max_dist, XC, YC, log_level=log_level)
+a = pyHIIexplorer(Ha_map, nx, ny, max_dist, frac_peak, F_max, dist, min_flux,
+                  obj_name, output_path, XC, YC, log_level=log_level)
+a.HIIrecover()
