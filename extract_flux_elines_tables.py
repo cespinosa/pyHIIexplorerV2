35a20724fc08a7bd787291e481e765d0c87a1e2a
cespinosa <cespinosa@astro.unam.mx>
2019-09-17 09:30:48 -0500

    removing fhwm_inst

--- extract_flux_elines_tables.py
+++ extract_flux_elines_tables.py
@@ -48,12 +48,12 @@ def extract_flux_elines_table(seg_map, fe_file, output, log_level):
         name = header['OBJECT']
         califaID = header['CALIFAID']
         # label_list = ('HIERARCH V500 PPAK P1 GRAT_ID',
-                      'HIERARCH V500 PPAK P3F1 GRAT_ID',
-                      'HIERARCH V500 PPAK P3F2 GRAT_ID',
-                      'HIERARCH V500 PPAK P3F3 GRAT_ID',
-                      'HIERARCH V500 PPAK P6F2 GRAT_ID',
-                      'HIERARCH V500 PPAK P7F11 GRAT_ID',
-                      'HIERARCH V500 PPAK P9F2 GRAT_ID')
+        #              'HIERARCH V500 PPAK P3F1 GRAT_ID',
+        #              'HIERARCH V500 PPAK P3F2 GRAT_ID',
+        #              'HIERARCH V500 PPAK P3F3 GRAT_ID',
+        #              'HIERARCH V500 PPAK P6F2 GRAT_ID',
+        #              'HIERARCH V500 PPAK P7F11 GRAT_ID',
+        #              'HIERARCH V500 PPAK P9F2 GRAT_ID')
         # for label in label_list:
         #     try:
         #         grat = header[label]
@@ -153,12 +153,12 @@ def extract_flux_elines_table(seg_map, fe_file, output, log_level):
             fp.write("#  COLUMN7:  DEC                ,"
                      + " float, degree  ,"
                      + " DEC of the Centroid of the HII region\n")
-            fp.write("#  COLUMN8:  Ha_VEL             ,"
-                     + "  float, km/s  , Ha velocity in km/s\n")
-            fp.write("#  COLUMN9:  Ha_DISP            ,"
-                     + "  float, km/s  ,"
-                     + " Ha velocity dispersion-sigma in km/s\n")
-            NC = 10
+            # fp.write("#  COLUMN8:  Ha_VEL             ,"
+            #          + "  float, km/s  , Ha velocity in km/s\n")
+            # fp.write("#  COLUMN9:  Ha_DISP            ,"
+            #          + "  float, km/s  ,"
+            #          + " Ha velocity dispersion-sigma in km/s\n")
+            NC = 8
             writer = csv.writer(fp)
             for i, wl in enumerate(wavelengths):
                 for I in [i, i+4*NZ, i+3*NZ, i+7*NZ]:
@@ -203,18 +203,18 @@ def extract_flux_elines_table(seg_map, fe_file, output, log_level):
                 a_out_vel = a_out_vel[indexs]
                 a_out_disp = a_out_disp[indexs]
                 a_lambda = wavelengths[indexs]                
-                vel_Ha = a_out_med[96, i]
-                disp_Ha = a_out_med[147, i]
-                disp_Ha = np.sqrt(abs(disp_Ha**2 - fwhm_inst**2))
-                disp_Ha = (disp_Ha/6563)*scts.speed_of_light/1000
-                a_out_disp = np.sqrt(abs(a_out_disp**2-fwhm_inst**2))
-                a_out_disp = (a_out_disp/a_lambda)*scts.speed_of_light/1000
-                a_out_disp[a_out_disp <= 10] = np.nan
-                vel = vel_Ha
-                disp = disp_Ha
-                if np.nanmin(a_out_disp) < disp:
-                    disp = np.nanmin(a_out_disp)
-                disp = disp/2.354
+                # vel_Ha = a_out_med[96, i]
+                # disp_Ha = a_out_med[147, i]
+                # disp_Ha = np.sqrt(abs(disp_Ha**2 - fwhm_inst**2))
+                # disp_Ha = (disp_Ha/6563)*scts.speed_of_light/1000
+                # a_out_disp = np.sqrt(abs(a_out_disp**2-fwhm_inst**2))
+                # a_out_disp = (a_out_disp/a_lambda)*scts.speed_of_light/1000
+                # a_out_disp[a_out_disp <= 10] = np.nan
+                # vel = vel_Ha
+                # disp = disp_Ha
+                # if np.nanmin(a_out_disp) < disp:
+                #     disp = np.nanmin(a_out_disp)
+                # disp = disp/2.354
                 Row_lines.append(HIIREGID)
                 Row_lines.append(name)
                 Row_lines.append(califaID)
@@ -222,8 +222,8 @@ def extract_flux_elines_table(seg_map, fe_file, output, log_level):
                 Row_lines.append(y[i])
                 Row_lines.append(RA)
                 Row_lines.append(DEC)
-                Row_lines.append(vel)
-                Row_lines.append(disp)
+                # Row_lines.append(vel)
+                # Row_lines.append(disp)
                 for j in np.arange(NZ):
                     for J in [j, j+4*NZ, j+3*NZ, j+7*NZ]:
                               # i+NZ, i+5*NZ, i+2*NZ, i+6*NZ]:
