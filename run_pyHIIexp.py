6662b9a39d336c6f04445c408f3834fdac7d24b8
cespinosa <cespinosa@astro.unam.mx>
2019-09-16 20:17:28 -0500

    adding functior to pCALIFA

--- run_pyHIIexp.py
+++ run_pyHIIexp.py
@@ -4,10 +4,15 @@ from extract_SFH_table import extract_SFH_table
 from extract_SSP_table import extract_SSP_table
 
 log_level='info'
-
+#eCALIFA
 fe_dir = '/home/espinosa/CALIFA_DATA/dataproducts/fe_files/'
 sfh_dir = '/home/espinosa/CALIFA_DATA/dataproducts/sfh_files/'
 ssp_dir = '/home/espinosa/CALIFA_DATA/dataproducts/ssp_files/'
+#pCALIFA
+p_fe_dir = '/home/espinosa/CALIFA_DATA/pCALIFA/fe_files_corr/'
+p_sfh_dir = '/home/espinosa/CALIFA_DATA/pCALIFA/sfh_files/'
+p_ssp_dir = '/home/espinosa/CALIFA_DATA/pCALIFA/ssp_files/'
+
 
 def extract_HIIregions(obj_name):
     max_dist=5.5
@@ -15,10 +20,31 @@ def extract_HIIregions(obj_name):
     F_max=0.3
     dist=0
     min_flux=0.05
-    output_path='/home/espinosa/tmp/'
+    output_path='/home/espinosa/CALIFA_DATA/eCALIFA/catalog/'
     fe_file = fe_dir + 'flux_elines.{}.cube.fits.gz'.format(obj_name)
     sfh_file = sfh_dir + '{}.SFH.cube.fits.gz'.format(obj_name)
     ssp_file = ssp_dir + '{}.SSP.cube.fits.gz'.format(obj_name)
+
+    a = pyHIIexplorer(fe_file, output_path, max_dist, frac_peak, F_max, dist,
+                      min_flux, log_level=log_level, n_index=45)
+    a.HIIrecover()
+
+    seg_map = output_path + "seg_Ha_EW.{}.fits.gz".format(obj_name)
+
+    extract_flux_elines_table(seg_map, fe_file, output_path, log_level)
+    extract_SFH_table(seg_map, sfh_file, output_path, log_level)
+    extract_SSP_table(seg_map, ssp_file, fe_file, output_path, log_level)
+
+def extract_HIIregions_p(obj_name):
+    max_dist=5.5
+    frac_peak=0.05
+    F_max=0.3
+    dist=0
+    min_flux=0.05
+    output_path='/home/espinosa/CALIFA_DATA/pCALIFA/catalog/'
+    fe_file = p_fe_dir + 'flux_elines.{}.cube.fits.gz'.format(obj_name)
+    sfh_file = p_sfh_dir + '{}.SFH.cube.fits.gz'.format(obj_name)
+    ssp_file = p_ssp_dir + '{}.SSP.cube.fits.gz'.format(obj_name)
 
     a = pyHIIexplorer(fe_file, output_path, max_dist, frac_peak, F_max, dist,
                       min_flux, log_level=log_level, n_index=45)
--- run_pyHIIexp_mp.py
+++ run_pyHIIexp_mp.py
@@ -4,7 +4,7 @@ import pandas as pd
 from multiprocessing import Pool
 from run_pyHIIexp import extract_HIIregions
 eCALIFA_path = '/home/espinosa/CALIFA_DATA/eCALIFA/'
-
+pCALIFA_path = '/home/espinosa/CALIFA_DATA/pCALIFA/'
 
 def make_catalog(obj_names):
     pool = Pool(4)
@@ -39,9 +39,45 @@ def get_names_get_proc_files_eCALIFA(path=None, clean_catalog=True):
         galaxy_names = df_clean.values.ravel()
     return galaxy_names
 
-def clean_sample():
+def get_names_pCALIFA():
+    path_files = glob.glob(pCALIFA_path + 'fe_files/'  + 'flux_elines.*')
+    obj_names = [path_file.split('.', 1)[1][:-13]
+             for path_file in path_files ]
+    return obj_names
+
+def get_names_get_proc_files_pCALIFA(path=None, clean_catalog=True):
+    # For PILOT sample, only one get_proc_elines table exists
+    if path is None:
+        path = pCALIFA_path
+    if clean_catalog is True:
+        name_file = path + 'get_proc_elines_PILOT.csv'
+        df_clean = pd.read_csv(name_file, comment='#', header=None,
+                               usecols=[0])
+        galaxy_names = df_clean.values.ravel()
+    elif clean_catalog is False:
+        name_file = path + 'get_proc_elines_PILOT.csv'
+        df = pd.read_csv(name_file, comment='#', header=None,
+                         usecols=[0])
+        galaxy_names = df.values.ravel()
+    elif clean_catalog is None:
+        print('No option for clean_catalog. Clean catalog is set by default')
+        name_file = path + 'get_proc_elines_PILOT.clean.csv'
+        df_clean = pd.read_csv(name_file, comment='#', header=None,
+                               usecols=[0])
+        galaxy_names = df_clean.values.ravel()
+    return galaxy_names
+
+def clean_sample_eCALIFA():
     fe_names = get_names_eCALIFA()
     galaxy_clean_names = get_names_get_proc_files_eCALIFA()
+    obj_names = np.intersect1d(galaxy_clean_names, fe_names)
+    obj_names_diff = np.setdiff1d(galaxy_clean_names, fe_names)
+    # print("# missing fe files: {}".format(len(obj_names_diff)))
+    return obj_names
+
+def clean_sample_pCALIFA():
+    fe_names = get_names_pCALIFA()
+    galaxy_clean_names = get_names_get_proc_files_pCALIFA()
     obj_names = np.intersect1d(galaxy_clean_names, fe_names)
     obj_names_diff = np.setdiff1d(galaxy_clean_names, fe_names)
     # print("# missing fe files: {}".format(len(obj_names_diff)))
