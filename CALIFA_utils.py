3cb599fc043aa8ff2ee1dad22b7fd8726ae2e92b
cespinosa <cespinosa@astro.unam.mx>
2020-09-14 11:29:38 -0500

    Remove logger package

--- CALIFA_utils.py
+++ CALIFA_utils.py
@@ -4,32 +4,21 @@ import csv
 from astropy.io import fits
 
 def get_slice_from_flux_elines(fe_file, n_param=45, header=True,
-                               log_level='info'):
-    logger = logging.getLogger('get slice from fe file')
-    logger.propagate = False
-    ch = logging.StreamHandler()
-    if log_level == 'info':
-        logger.setLevel(level=logging.INFO)
-        ch.setLevel(logging.INFO)
-    if log_level == 'debug':
-        logger.setLevel(level=logging.DEBUG)
-        ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(levelname)s %(name)s: %(message)s')
-    ch.setFormatter(formatter)
-    if (logger.hasHandlers()):
-        logger.handlers.clear()
-    logger.addHandler(ch)
+                               verbose=False):
     if header:
-        logger.debug('Getting data and header from fits file')
+        if verbose:
+            print('Getting data and header from fits file')
         head, data = read_flux_elines_cubes(fe_file,
-                                            header=header, log_level=log_level)
+                                            header=header, verbose=verbose)
     else:
-        logger.debug('Getting only data from fits file')
+        if verbose:
+            print('Getting only data from fits file')
         data = read_flux_elines_cubes(fe_file,
-                                      header=header, log_level=log_level)
+                                      header=header, verbose=verbose)
     
     if data is not None:
-        logger.debug('Getting cube slice {}'.format(n_param))
+        if verbose:
+            print('Getting cube slice {}'.format(n_param))
         map_slice = data[n_param, :, :]
         if header:
             return head, map_slice
@@ -41,90 +30,53 @@ def get_slice_from_flux_elines(fe_file, n_param=45, header=True,
         else:
             return None
 
-def read_fits_file(fits_file, header=True, log_leve='info'):
-    logger = logging.getLogger('read fits file')
-    logger.propagate = False
-    ch = logging.StreamHandler()
-    if log_level == 'info':
-        logger.setLevel(level=logging.INFO)
-        ch.setLevel(logging.INFO)
-    if log_level == 'debug':
-        logger.setLevel(level=logging.DEBUG)
-        ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(levelname)s %(name)s: %(message)s')
-    ch.setFormatter(formatter)
-    if (logger.hasHandlers()):
-        logger.handlers.clear()
-    logger.addHandler(ch)
-    logger.info("Reading {}".format(fits_file))
+def read_fits_file(fits_file, header=True, verbose=False):
+    print("Reading {}".format(fits_file))
     if os.path.isfile(fits_file):
         if header:
             data, header = fits.getdata(fits_file, header=header)
-            logger.debug('Read fits file Done')
+            if verbose:
+                print('Read fits file Done')
             return header, data
         else:
             data = fits.getdata(fits_file, header=header)
-            logger.debug('Read fits file Done')
+            if verbose:
+                print('Read fits file Done')
             return data
     else:
-        logger.error('fits file not found')
+        print('fits file not found')
         if header:
             return None, None
         else:
             return None
 
-def read_flux_elines_cubes(fe_file, header=True, log_level='info'):
-    logger = logging.getLogger('read fe file')
-    logger.propagate = False
-    ch = logging.StreamHandler()
-    if log_level == 'info':
-        logger.setLevel(level=logging.INFO)
-        ch.setLevel(logging.INFO)
-    if log_level == 'debug':
-        logger.setLevel(level=logging.DEBUG)
-        ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(levelname)s %(name)s: %(message)s')
-    ch.setFormatter(formatter)
-    if (logger.hasHandlers()):
-        logger.handlers.clear()
-    logger.addHandler(ch)
-    logger.info("Reading {}".format(fe_file))
+def read_flux_elines_cubes(fe_file, header=True, verbose=False):
+    print("Reading flux eline cube: {}".format(fe_file))
     if os.path.isfile(fe_file):
         if header:
             data, header = fits.getdata(fe_file, header=header)
-            logger.debug('Read flux elines done')
+            if verbose:
+                print('Read flux elines done')
             return header, data
         else:
             data = fits.getdata(fe_file, header=header)
-            logger.debug('Read flux elines done')
+            if verbose:
+                print('Read flux elines done')
             return data
     else:
-        logger.error('file not found')
+        print('File not found: {}'.format(fe_file))
         if header:
             return None, None
         else:
             return None
 
-def get_center(obj_name, log_level='info'):
+def get_center(obj_name, verbose=False):
     XC = 0
     YC = 0
     dir_path = '/home/espinosa/CALIFA_DATA/eCALIFA/'
-    file_name = 'get_proc_elines_CALIFA.clean.csv'    
-    logger = logging.getLogger('get center')
-    logger.propagate = False
-    ch = logging.StreamHandler()
-    if log_level == 'info':
-        logger.setLevel(level=logging.INFO)
-        ch.setLevel(logging.INFO)
-    if log_level == 'debug':
-        logger.setLevel(level=logging.DEBUG)
-        ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(levelname)s %(name)s: %(message)s')
-    ch.setFormatter(formatter)
-    if (logger.hasHandlers()):
-        logger.handlers.clear()
-    logger.addHandler(ch)
-    logger.debug("File path {}".format(dir_path + file_name))
+    file_name = 'get_proc_elines_CALIFA.clean.csv'
+    if verbose:
+        print("File path {}".format(dir_path + file_name))
     try:
         with open(dir_path + file_name, 'r') as fileCAL:
             reader = csv.reader(fileCAL)
@@ -134,32 +86,18 @@ def get_center(obj_name, log_level='info'):
                     YC = float(row[168])
                     break
         if XC == 0:
-            logger.warn('{} is not in'.format(obj_name) + 
-                           'get_proc_elines_CALIFA.csv file')
-            logger.warn('Setting XC=YC=0')
+            print('{} is not in'.format(obj_name) +
+                  'get_proc_elines_CALIFA.csv file')
+            print('Setting XC=YC=0')
         else:
-            logger.debug('{} found in get_proc_elines'.format(obj_name))
+            print('{} found in get_proc_elines'.format(obj_name))
     except FileNotFoundError:
-        logger.error('get_proc_elines_CALIFA.csv not found')
+        print('get_proc_elines_CALIFA.csv not found')
     except:
-        logger.warn('Something wrong  with get center function')
+        print('Something wrong  with get center function')
     return XC, YC
 
-def read_seg_map(seg_map, header=False, log_level='info'):
-    logger = logging.getLogger('read seg map')
-    logger.propagate = False
-    ch = logging.StreamHandler()
-    if log_level == 'info':
-        logger.setLevel(level=logging.INFO)
-        ch.setLevel(logging.INFO)
-    if log_level == 'debug':
-        logger.setLevel(level=logging.DEBUG)
-        ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(levelname)s %(name)s: %(message)s')
-    ch.setFormatter(formatter)
-    if (logger.hasHandlers()):
-        logger.handlers.clear()
-    logger.addHandler(ch)
+def read_seg_map(seg_map, header=False, verbose=True):
     file_path = seg_map
     if os.path.isfile(file_path):
         if header:
@@ -167,32 +105,18 @@ def read_seg_map(seg_map, header=False, log_level='info'):
         else:
             data = fits.getdata(file_path, header=header)
     else:
-        logger.warn('No exist segmentation map file')
+        print('No exist segmentation map file: {}'.format(seg_map))
         if header:
             return None, [0]
         else:
             [0]
-    logger.info('Read Segmentation map file done')
+    print('Read Segmentation map file done')
     if header:
         return header, data
     else:
         return data
 
-def read_mask_map(mask_map, header=False, log_level='info'):
-    logger = logging.getLogger('read mask map')
-    logger.propagate = False
-    ch = logging.StreamHandler()
-    if log_level == 'info':
-        logger.setLevel(level=logging.INFO)
-        ch.setLevel(logging.INFO)
-    if log_level == 'debug':
-        logger.setLevel(level=logging.DEBUG)
-        ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(levelname)s %(name)s: %(message)s')
-    ch.setFormatter(formatter)
-    if (logger.hasHandlers()):
-        logger.handlers.clear()
-    logger.addHandler(ch)
+def read_mask_map(mask_map, header=False, verbose=False):
     file_path = mask_map
     if os.path.isfile(file_path):
         if header:
@@ -200,75 +124,49 @@ def read_mask_map(mask_map, header=False, log_level='info'):
         else:
             data = fits.getdata(file_path, header=header)
     else:
-        logger.warn('No exist masked map file')
+        print('No exist masked map file')
         if header:
             return None, [0]
         else:
             [0]
-    logger.info('Read masked map file done')
+    print('Read masked map file done')
     if header:
         return header, data
     else:
         return data
 
     
-def read_SSP_fits(ssp_file, header=True, log_level='info'):
-    logger = logging.getLogger('read ssp file')
-    logger.propagate = False
-    ch = logging.StreamHandler()
-    if log_level == 'info':
-        logger.setLevel(level=logging.INFO)
-        ch.setLevel(logging.INFO)
-    if log_level == 'debug':
-        logger.setLevel(level=logging.DEBUG)
-        ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(levelname)s %(name)s: %(message)s')
-    ch.setFormatter(formatter)
-    if (logger.hasHandlers()):
-        logger.handlers.clear()
-    logger.addHandler(ch)
-    logger.info("Reading SSP cube: {}".format(ssp_file))
+def read_SSP_fits(ssp_file, header=True, verbose=False):
+    print("Reading SSP cube: {}".format(ssp_file))
     file_path = ssp_file
     if os.path.isfile(file_path):
         try:
             if header:
                 data, head = fits.getdata(file_path, header=header)
-                logger.debug('Read SSP done')
+                if verbose:
+                    print('Read SSP done')
                 return head, data
             else:
                 data = fits.getdata(file_path, header=header)
-                logger.debug('Read SSP done')
+                if verbose:
+                    print('Read SSP done')
                 return data
         except Exception:
-            logger.warn('Something wrong with SSP FITS file for' +
-                           '{}'.format(obj_name))
+            print('Something wrong with SSP FITS file for' +
+                  '{}'.format(obj_name))
             if header:
                 return None, None
             else:
                 return None
     else:
-        logger.error('SSP Fits file not found for {}'.format(obj_name))
+        print('SSP Fits file not found for {}'.format(obj_name))
         if header:
             return None, None
         else:
             return None
 
-def read_SFH_fits(sfh_file, header=True, log_level='info'):
-    logger = logging.getLogger('read sfh file')
-    logger.propagate = False
-    ch = logging.StreamHandler()
-    if log_level == 'info':
-        logger.setLevel(level=logging.INFO)
-        ch.setLevel(logging.INFO)
-    if log_level == 'debug':
-        logger.setLevel(level=logging.DEBUG)
-        ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(levelname)s %(name)s %(message)s')
-    ch.setFormatter(formatter)
-    if (logger.hasHandlers()):
-        logger.handlers.clear()
-    logger.addHandler(ch)
-    logger.info("Reading SFH cube: {}".format(sfh_file))
+def read_SFH_fits(sfh_file, header=True, verbose=False):
+    print("Reading SFH cube: {}".format(sfh_file))
     file_path = sfh_file
     if os.path.isfile(file_path):
         try:
@@ -281,13 +179,13 @@ def read_SFH_fits(sfh_file, header=True, log_level='info'):
             else:
                 return data
         except Exception:
-            logger.warn('Something wrong with SFH FITS file')
+            print('Something wrong with SFH FITS file')
             if header:
                 return None, None
             else:
                 return None
     else:
-        logger.error('SFH Fits file not found')
+        print('SFH Fits file not found')
         if header:
             return None, None
         else:
